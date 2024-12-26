396a3625ed6e95da7f7b6999d608b93b8f0f6e27
mdshahadatalamm <mdshahadatalam25@gmail.com>
2024-12-26 12:18:15 -0800

    added_Mongooes

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 var cors = require('cors')
 const app = express()
+const mongoose = require('mongoose');
 
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
 
 app.use(cors())
+app.use(express.json())
+
+
 
 app.get('/', function (req, res) {
   res.send('Hello World')
