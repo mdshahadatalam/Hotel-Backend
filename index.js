d7c0d28d86aad63c8b2a7dd1697404cbb248e384
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-02-05 21:24:32 -0800

    edit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 var cors = require('cors')
 const app = express()
+const port = 3000
 const mongoose = require('mongoose');
 const Banner = require('./Model/Banner')
 const Room = require('./Model/Room')
@@ -100,7 +101,13 @@ app.get('/serviceItem',async function(req,res){
 
 
 
-app.listen(3000)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
 
